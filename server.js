5a0333f81ac3ae7cb8fdcda6189c5e7033eb64eb
apoorv tikalkar <apoorvtikalkar@gmail.com>
2018-02-21 22:43:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,56 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone = {
+    title:'article one',
+    heading:'Article One',
+    date:'5 sept 2017',
+    content:` <p>Apple Inc. is an American multinational technology company headquartered in Cupertino, California that designs, develops, and sells consumer electronics, computer software, and online services. The company's hardware products include the iPhone smartphone, the iPad tablet computer, the Mac personal computer, the iPod portable media player, the Apple Watch smartwatch, the Apple TV digital media player, and the HomePod smart speaker. Apple's software includes the macOS and iOS operating systems, the iTunes media player, the Safari web browser, and the iLife and iWork creativity and productivity suites, as well as professional applications like Final Cut Pro, Logic Pro, and Xcode. Its online services include the iTunes Store, the iOS App Store and Mac App Store, Apple Music, and iCloud.
+    </p>
+    <p>Apple Inc. is an American multinational technology company headquartered in Cupertino, California that designs, develops, and sells consumer electronics, computer software, and online services. The company's hardware products include the iPhone smartphone, the iPad tablet computer, the Mac personal computer, the iPod portable media player, the Apple Watch smartwatch, the Apple TV digital media player, and the HomePod smart speaker. Apple's software includes the macOS and iOS operating systems, the iTunes media player, the Safari web browser, and the iLife and iWork creativity and productivity suites, as well as professional applications like Final Cut Pro, Logic Pro, and Xcode. Its online services include the iTunes Store, the iOS App Store and Mac App Store, Apple Music, and iCloud.
+    </p>
+    <p>Apple Inc. is an American multinational technology company headquartered in Cupertino, California that designs, develops, and sells consumer electronics, computer software, and online services. The company's hardware products include the iPhone smartphone, the iPad tablet computer, the Mac personal computer, the iPod portable media player, the Apple Watch smartwatch, the Apple TV digital media player, and the HomePod smart speaker. Apple's software includes the macOS and iOS operating systems, the iTunes media player, the Safari web browser, and the iLife and iWork creativity and productivity suites, as well as professional applications like Final Cut Pro, Logic Pro, and Xcode. Its online services include the iTunes Store, the iOS App Store and Mac App Store, Apple Music, and iCloud.
+    </p>`
+};
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+        var date=data.date;
+    var content=data.content;
 
+    
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+            ${title} 
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+               <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+<body>
+    <div class="container">
+    <div>
+         <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h2>${heading}</h2>
+    <div>
+        ${date}
+    </div>
+    <div>
+    ${content}
+    </div>
+        </div>
+  </body>
+ </html>
+
+
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -15,7 +64,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article-one',function(req,res)
 {
 
-      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+      res.send(createTemplate(articleone));
 
 });
 
