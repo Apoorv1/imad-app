1823049d79c7bc568ce2ddcc375afcc490a3af01
apoorv tikalkar <apoorvtikalkar@gmail.com>
2018-02-21 22:00:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.get('/article-one',function(req,res)
       res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });
+
 app.get('/article-two',function(req,res)
 {
-    res.send('Article two is displayed here');
+      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res)
 {
-    res.send('Article three is displayed here');
+      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
