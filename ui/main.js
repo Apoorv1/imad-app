3343463305e0145cbe70ea3a660b6c90004eaf75
apoorv tikalkar <apoorvtikalkar@gmail.com>
2018-02-22 09:30:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML="this is the new web app";
 //moving the image madi
 var img = document.getElementById('madi');
+var marginLeft=0;
+
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-img.style.marginLeft='100px';
+var interval=setInterval(moveRight,100);
 };
