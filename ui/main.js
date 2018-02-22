ed59710c5c0d3de2c89575b92525339f179b43ed
apoorv tikalkar <apoorvtikalkar@gmail.com>
2018-02-22 18:43:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 //changing the text of index.html
 var element = document.getElementById('main-text');
 element.innerHTML="this is the new web app";
@@ -14,3 +14,14 @@ function moveRight()
 img.onclick=function(){
 var interval=setInterval(moveRight,50);
 };*/
+var button=document.getElementById('counter');
+button.onclick=function(){
+  
+  
+  
+  
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
+  
+};
