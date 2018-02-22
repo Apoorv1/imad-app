684132c2b13b11f1f8bb438233ede2d3dae82f17
apoorv tikalkar <apoorvtikalkar@gmail.com>
2018-02-22 09:25:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //changing the text of index.html
 var element = document.getElementById('main-text');
 element.innerHTML="this is the new web app";
+//moving the image madi
+var img = document.getElementById('madi');
+img.onclick=function(){
+img.style.marginLeft='100px';
+};
